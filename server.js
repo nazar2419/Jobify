e41f6be095248822c04f81fe9e10400b7d5fe4ae
nazar2419 <nazarx095@gmail.com>
2022-08-22 22:05:56 +0300

    error handling

--- server.js
+++ server.js
@@ -2,15 +2,27 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+import 'express-async-errors';
+
+//db and authenticateUser
+import connectDB from './db/connect.js';
+
+// routers
+import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
 
 // middleware 
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
-import connectDB from './db/connect.js';
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome!');
 });
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
