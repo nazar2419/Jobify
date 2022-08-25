67c9c61a678452cd4f6eba21df9c48cd8863f0ab
nazar2419 <nazarx095@gmail.com>
2022-08-25 22:23:14 +0300

    hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ const login = async (req, res) => {
   res.send('login user');
 }
 const updateUser = async (req, res) => {
-  res.send('updateUser user')
+  res.send('updateUser user');
 }
 
 export { register, login, updateUser }
