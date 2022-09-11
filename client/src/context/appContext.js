c204897e5e340ccf1f17f088771925d85fb7376d
nazar2419 <nazarx095@gmail.com>
2022-09-11 23:20:03 +0300

    axios interceptors

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -34,6 +34,35 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // axios
+  const authFetch = axios.create({
+    baseURL:'/api/v1',
+  })
+
+  // request 
+  authFetch.interceptors.request.use(
+    (config) => {
+      config.headers.common['Authorization'] = `Bearer ${state.token}`;
+      return config;
+    }, (error) => {
+      return Promise.reject(error);
+    }
+  )
+  // response 
+
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error.response);
+      if (error.response.status === 401) {
+        console.log('AUTH ERROR');
+      }
+      return Promise.reject(error);
+    }
+  )
+
   const displayAlert = () => {
     dispatch({type: DISPLAY_ALERT});
     clearAlert();
@@ -87,10 +116,25 @@ const AppProvider = ({ children }) => {
     dispatch({type:LOGOUT_USER});
     removeUserFromLocalStorage();
   }
+  const updateUser = async (currentUser) => {
+    try {
+      const {data} = await authFetch.patch('/auth/updateUser',currentUser);
+      console.log(data);
+    } catch (error) {
+      // console.log(error.response);
+    }
+  }
 
   return (
     <AppContext.Provider 
-      value={{...state, displayAlert, setupUser, toggleSidebar, logoutUser }}>
+      value={{
+        ...state, 
+        displayAlert, 
+        setupUser, 
+        toggleSidebar, 
+        logoutUser,
+        updateUser 
+      }}>
       {children}
     </AppContext.Provider>
   )
