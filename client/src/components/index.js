a679072c06b1df09c5ba36facd6972a7648d6dd0
nazar2419 <nazarx095@gmail.com>
2022-09-14 21:33:57 +0300

    create job frond-end

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,9 @@
 import Logo from "./Logo";
 import FormRow from "./FormRow";
+import FormRowSelect from "./FormRowSelect";
 import Alert from './Alert';
 import BigSidebar from "./BigSidebar";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
 
-export { Logo, FormRow, Alert, Navbar, BigSidebar, SmallSidebar };
+export { Logo, FormRow, Alert, Navbar, BigSidebar, SmallSidebar, FormRowSelect };
