98c1888d28caf870c4da6e5f5a68566cb473176b
nazar2419 <nazarx095@gmail.com>
2022-09-26 00:04:53 +0300

    monthly applications - frond-end

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,9 +1,21 @@
+import React, { useState } from "react"
 
-const ChartsContainer = () => {
+import BarChartComponent from "./BarChartComponent"
+import AreaChartComponent from "./AreaChartComponent"
+import { useAppContext } from "../context/appContext"
+import Wrapper from "../assets/wrappers/ChartsContainer"
+
+export default function ChartsContainer() {
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+
   return (
-    <div>
-      <h1>charts container</h1>
-    </div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'AreaChart' : 'BarChart'}
+      </button>
+      {barChart ? <BarChartComponent data={data}/> : <AreaChartComponent data={data} />}
+    </Wrapper>
   )
 }
-export default ChartsContainer
