dac746e31942fe434a2eca73b51325e0fec3362a
nazar2419 <nazarx095@gmail.com>
2022-12-07 01:00:40 +0200

    security packages

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+
 //db and authenticateUser
 import connectDB from './db/connect.js';
 
@@ -12,42 +20,45 @@ import connectDB from './db/connect.js';
 import authRouter from './routes/authRoutes.js';
 import jobsRouter from './routes/jobsRoutes.js';
 
-// middleware 
+// middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
-import authenticateUser from './middleware/auth.js'
+import authenticateUser from './middleware/auth.js';
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
-app.get('/', (req, res) => {
-  res.json({msg: 'Welcome!'});
-});
-app.get('/api/v1', (req, res) => {
-  res.json({msg: 'Api!'});
-});
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000
-
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
-      console.log(`Server oslistening on port ${port}...`)}
-    )
+      console.log(`Server oslistening on port ${port}...`);
+    });
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 start();
